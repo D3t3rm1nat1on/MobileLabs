afb52b2cebd22cdd8552cadf3a5b794d503ef53b
D3t3rm1nat1on <bro.chek@mail.ru>
2021-03-12 20:13:37 +0300

    nice

---
+++ Lesson1/clickbuttons/src/main/java/com/mirea/chekushin/clickbuttons/MainActivity.java
@@ -0,0 +1,31 @@
+package com.mirea.chekushin.clickbuttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView printF = findViewById(R.id.printF);
+    private Button btnOk = findViewById(R.id.btnOk);
+    private Button btnCancel = findViewById(R.id.btnOk);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View.OnClickListener onClickListener = v -> {
+           printF.setText("Ты красавчик");
+        };
+        btnOk.setOnClickListener(onClickListener);
+    }
+
+    public void onCancel(View view) {
+        Toast.makeText(this, "Уже не красавчик", Toast.LENGTH_SHORT).show();
+    }
+}
