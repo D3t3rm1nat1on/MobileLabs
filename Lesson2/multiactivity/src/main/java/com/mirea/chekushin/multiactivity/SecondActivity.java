f7a28ee3855c5558b3c9c1f5f0da60f64de52549
D3t3rm1nat1on <bro.chek@mail.ru>
2021-05-21 12:57:46 +0300

    lesson2

---
+++ Lesson2/multiactivity/src/main/java/com/mirea/chekushin/multiactivity/SecondActivity.java
@@ -0,0 +1,36 @@
+package com.mirea.chekushin.multiactivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        String text = (String) getIntent().getSerializableExtra("key");
+        setContentView(R.layout.activity_second);
+        TextView t_view = (TextView) findViewById(R.id.textView_secondActivity);
+        t_view.setText(text);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
