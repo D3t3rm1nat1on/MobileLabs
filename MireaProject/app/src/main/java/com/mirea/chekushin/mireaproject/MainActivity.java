6ad07bc694e31304829f9f2e35ac00a66758ddeb
D3t3rm1nat1on <bro.chek@mail.ru>
2021-06-06 18:15:21 +0300

    LESSON 7

--- MireaProject/app/src/main/java/com/mirea/chekushin/mireaproject/MainActivity.java
+++ MireaProject/app/src/main/java/com/mirea/chekushin/mireaproject/MainActivity.java
@@ -1,9 +1,6 @@
 package com.mirea.chekushin.mireaproject;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -43,12 +40,15 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_browser, R.id.nav_calculator, R.id.nav_hardware, R.id.nav_options, R.id.nav_stories)
+                R.id.nav_home, R.id.nav_browser, R.id.nav_calculator, R.id.nav_hardware, R.id.nav_options, R.id.nav_stories, R.id.nav_info)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        Intent intent = new Intent(this, FirebaseActivity.class);
+        startActivity(intent);
     }
 
     @Override
@@ -67,11 +67,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickPlayMusic(View view) {
         startService(
-                new Intent(MainActivity.this, PlayerService.class));
+                new Intent(this, PlayerService.class));
     }
 
     public void onClickStopMusic(View view) {
         stopService(
-                new Intent(MainActivity.this, PlayerService.class));
+                new Intent(this, PlayerService.class));
     }
 }
